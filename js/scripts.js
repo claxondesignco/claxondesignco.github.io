8977b1150390bdc6265005e2804ba57d3cb4feed
Adam Claxon <hi@adamclaxon.com>
2017-02-12 21:01:15 -0500

    About section

--- js/scripts.js
+++ js/scripts.js
@@ -26,41 +26,65 @@ $(function() {
 
 $("#simplicity").waypoint(function(direction){
   if (direction === 'down') {
-    $('a').removeClass("active");
+    $('a').not("[href='#about']").removeClass("active");
     $("[href='#simplicity']").addClass("active");
   }
 });
 
 $("#simplicity").waypoint(function(direction) {
   if (direction === 'up') {
-    $('a').removeClass("active");
+    $('a').not("[href='#about']").removeClass("active");
   }
 });
 
 $("#work").waypoint(function(direction){
   if (direction === 'down') {
-    $('a').removeClass("active");
+    $('a').not("[href='#about']").removeClass("active");
     $("[href='#work']").addClass("active");
   }
 });
 
 $("#work").waypoint(function(direction) {
   if (direction === 'up') {
-    $('a').removeClass("active");
+    $('a').not("[href='#about']").removeClass("active");
     $("[href='#simplicity']").addClass("active");
   }
 });
 
 $("#ethics").waypoint(function(direction){
   if (direction === 'down') {
-    $('a').removeClass("active");
+    $('a').not("[href='#about']").removeClass("active");
     $("[href='#ethics']").addClass("active");
   }
 });
 
 $("#ethics").waypoint(function(direction) {
   if (direction === 'up') {
-    $('a').removeClass("active");
+    $('a').not("[href='#about']").removeClass("active");
     $("[href='#work']").addClass("active");
+  }
+});
+
+$(".navigation-list-item-link").not("[href='#about']").click(function(){
+  $("#about-section").removeClass("open");
+  $("[href='#about']").removeClass("active");
+  $("body").removeClass("no-scroll");
+  $("nav").removeClass('open');
+  $(".menu-button").removeClass('active');
+});
+
+$("#about-toggle").not(".active").click(function(){
+    $("#about-section-placeholder").load("about.html");
+    $( this ).addClass("active");
+    $("body").addClass("no-scroll");
+    $("nav").removeClass('open');
+    $(".menu-button").removeClass('active');
+});
+
+$('html').click(function(e) {
+  if (e.target.id != 'about-toggle' && 'about-section' && $(e.target).parents('#about-section').length == 0) {
+    $("#about-section").removeClass("open");
+    $("[href='#about']").removeClass("active");
+    $("body").removeClass("no-scroll");
   }
 });
