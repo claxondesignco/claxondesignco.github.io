7eb947a61197b2b48f0bed17c49255a3a9d966ce
Adam Claxon <hi@adamclaxon.com>
2017-07-15 13:13:03 -0400

    Moved about section to page; removed email address from navigation
    Removed css for About overlay; created layout for internal pages;
    removed JS for About section overlay toggle.

--- js/scripts.js
+++ js/scripts.js
@@ -92,23 +92,6 @@ $(".navigation-list-item-link").not("[href='#about']").click(function(){
   $(".menu-button").removeClass('active');
 });
 
-$("#about-toggle").click(function(){
-    $("#about-section").toggleClass("open");
-    $( this ).toggleClass("active");
-    $("body").toggleClass("no-scroll");
-    $("nav").toggleClass('open');
-    $(".menu-button").toggleClass('active');
-});
-
-$('html').click(function(e) {
-  if (e.target.id != 'about-toggle' && 'about-section' && $(e.target).parents('#about-section').length == 0) {
-    $("#about-section").removeClass("open");
-    $("[href='#about']").removeClass("active");
-    $("body").removeClass("no-scroll");
-  }
-});
-
-
 $('input[type="text"], input[type="email"], input[type="tel"], input[type="url"]').focus( function() {
   $(this).parent().addClass('active');
 });
