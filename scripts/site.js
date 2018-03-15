53f209e23f3c5e9d8e207f0cadb48865e87e49d9
Adam Claxon <adamclaxon@users.noreply.github.com>
2018-03-15 02:37:49 -0400

    Added initial (and probably final) script for form functionality.

--- scripts/site.js
+++ scripts/site.js
@@ -9,3 +9,13 @@ $("#toggle").click(function() {
     return attr == 'true' ? 'false' : 'true'});
   $( "#menu" ).slideToggle( function() {});
 });
+
+$('input[type="text"], input[type="email"], input[type="tel"], input[type="url"]').focus( function() {
+  $(this).parent().addClass('active');
+});
+
+$('input[type="text"], input[type="email"], input[type="tel"], input[type="url"]').blur( function() {
+  if( $(this).val().length === 0 ) {
+    $(this).parent().removeClass('active');
+  }
+});
