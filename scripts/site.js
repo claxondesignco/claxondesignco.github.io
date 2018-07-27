fc28c4dc985bf493488ed6d863e16d372ec83fed
Adam Claxon <adamclaxon@users.noreply.github.com>
2018-07-26 23:02:52 -0400

    Created Ajax loading of all pages.

--- scripts/site.js
+++ scripts/site.js
@@ -12,7 +12,6 @@ $("#toggle").click(function() {
   $( this ).attr('aria-expanded', function (i, attr) {
     return attr == 'true' ? 'false' : 'true'});
   $( "#menu" ).slideToggle( function() {});
-  $( "header[role=banner]" ).toggleClass("expanded");
 });
 
 $('input[type="text"], input[type="email"], input[type="tel"], input[type="url"]').focus( function() {
@@ -24,3 +23,27 @@ $('input[type="text"], input[type="email"], input[type="tel"], input[type="url"]
     $(this).parent().removeClass('active');
   }
 });
+
+function LoadWorkSample() {
+  var page = $(this).attr('href');        
+  $('footer[role="contentinfo"]').fadeOut(1);
+  $('main').fadeOut(400, function() {
+    $('footer[role="contentinfo"]').fadeOut(1);
+    $('main').load(page + " main > *", function() {
+      $(this).delay(100).fadeIn(400);
+      window.history.pushState("", "", page);
+      $('footer[role="contentinfo"]').delay(100).fadeIn(400);
+    });
+  });
+  $('title').load(page + ' title', function(data) {
+    document.title = $(this).text();
+  });
+  $('#toggle').attr('aria-expanded', 'false');
+  $( "#menu" ).slideUp();
+  return false;       
+}
+
+$('body').on('click','header a', LoadWorkSample);
+$('main').on('click','a.portfolio-item-link', LoadWorkSample);
+$('main').on('click','a.prev', LoadWorkSample);
+$('main').on('click','a.next', LoadWorkSample);
